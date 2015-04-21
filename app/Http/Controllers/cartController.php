bdf4c272c50626f2da58b07094a10fef3a5b0d1c
carlAkerlund <carl.akerlund@outlook.com>
2015-04-21 16:05:06 +0200

    ****

--- app/Http/Controllers/cartController.php
+++ app/Http/Controllers/cartController.php
@@ -16,8 +16,8 @@ class cartController extends Controller {
    */
   public function index()
   {
-    Cart::add('192ao12', 'Product 1', 1, 9.99);
-    Cart::add('1239ad0', 'Product 2', 2, 5.95, array('size' => 'large'));
+    // Cart::add('192ao12', 'Product 1', 1, 9.99);
+    // Cart::add('1239ad0', 'Product 2', 2, 5.95, array('size' => 'large'));
 
     $cart = Cart::content();
 
