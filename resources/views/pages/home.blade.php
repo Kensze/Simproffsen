3b81f90a835a8d20abd9d24af0ec3fda26ef65e4
carlAkerlund <carl.akerlund@outlook.com>
2015-04-21 15:55:08 +0200

    old commit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,17 +1,15 @@
 @extends('app')
 
 @section('content')
-
+<h1>Hejsan</h1>
 <div class="products_container">
-	<div id="product_item">	
-		{!! HTML::image('img/jacka.jpg', 'alt', array( 'class' => "product_img" )) !!}
-		<div class="product_price">
-			alsdjfladfhjaskdfhkj
-		</div>
-		<div class="product_buttons">
-			<span class="glyphicon glyphicon-shopping-cart" id="to_cart"></span>
-			<span class="glyphicon glyphicon-heart" id="fav"></span>
-		</div>
+	<div id="product_item">
+
+
+	<div id="product_img">
+		{!! HTML::image('img/jacka.jpg')!!}
+
+	</div>
 	</div>
 </div>
-@endsection
+@stop
